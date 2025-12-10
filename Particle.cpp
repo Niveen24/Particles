0d5ac77e6e600ead561858976cacd4e662cd7b17
Niveen24 <niveensaeed06@gmail.com>
2025-12-09 21:47:01 -0800

    particle.cpp all functions defined 9:46pm 12/9

--- Particle.cpp
+++ Particle.cpp
@@ -183,10 +183,52 @@ Particle::Particle(RenderTarget& target, int numPoints, Vector2i mouseClickPosit
 
 void Particle::draw(RenderTarget& target, RenderStates states) const    //this is an override
 {
-    
+    VertexArray lines(TriangleFan, m_numPoints + 1);
+    Vector2f center(target.mapCoordsToPixel(m_centerCoordinate, m_cartesianPlane));
+    lines[0].position = center;
+	lines[0].color = m_color1;
+
+    for (int j = 1; j <= m_numPoints; j++)
+    {
+		lines[j].position = target.mapCoordsToPixel(Vector2f(m_A(0, j - 1), m_A(1, j - 1)), m_cartesianPlane); //map from Cartesian to screen coords
+		lines[j].color = m_color2;
+    }
+    target.draw(lines);
 }
 
 void Particle::update(float dt)
 {
+    m_ttl -= dt;
+	rotate(dt * m_radiansPerSec);
+    scale(SCALE);
+    float dx, dy;
+	m_vy -= G * dt; //gravity effect
+    dy = m_vy * dt;
+    translate(dx, dy);
+}
 
+void Particle::rotate(double theta)
+{
+	Vector2f temp = m_centerCoordinate;
+	translate(-m_centerCoordinate.x, -m_centerCoordinate.y);
+    RotationMatrix R(theta);
+    m_A = R * m_A;
+    translate(temp.x, temp.y);
+}
+
+void Particle::scale(double c)
+{
+    Vector2f temp = m_centerCoordinate;
+    translate(-m_centerCoordinate.x, -m_centerCoordinate.y);
+    ScalingMatrix S(c);
+    m_A = S * m_A;
+    translate(temp.x, temp.y);
+}
+
+void Particle::translate(double xShift, double yShift)
+{
+    TranslationMatrix T(xShift, yShift, m_A.getCols());
+    m_A = T + m_A;
+    m_centerCoordinate.x += xShift;
+    m_centerCoordinate.y += yShift;
 }
